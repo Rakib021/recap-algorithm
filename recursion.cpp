5f28b5f58d7d5e6de34f7c6c7e8093fbefbd822d
Rakib <rakib@Rakibs-MacBook-Pro.local>
2023-05-18 20:25:12 +0600

    dfs binary tree and normal tree

--- recursion.cpp
+++ recursion.cpp
@@ -1,14 +1,47 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int sum(int n){
+//     if(n==1){
+//         return 1;
+//     }
+//     return sum(n-1)+n;
+// }
+// int main(){
+
+// int n;
+// cin>>n;
+// cout<<"sum is ="<<sum(n)<<endl;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
-int sum(int n){
-    if(n==1){
-        return 1;
-    }
-    return sum(n-1)+n;
-}
-int main(){
 
-int n;
-cin>>n;
-cout<<"sum is ="<<sum(n)<<endl;
+int sum(int n){
+    if(n==1) return 1;
+    
+    return sum(n-1) +n;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    cout<<"sum ="<<sum(n)<<endl;
+
 }
